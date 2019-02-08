1795d729f5ba39ff3c3f2091633bc62377f4cd7b
Joao <joaoded@hotmail.com>
2019-02-08 15:14:18 -0200

    Add sample

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
-    println!("Hello, world!");
-    print_sum(5,5)
+    // println!("Hello, world!");
+    // print_sum(5,5)
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
 }
 
 fn print_sum(a: i8, b: i8) {
